f6c0138f9805f794d5cd102e1755f2d0436bbd8f
Thomas Blum <ga96xic@mytum.de>
2020-01-08 16:13:48 +0100

    Clean up test_louds_sparse and remove serialize test

--- test/unitTest/test_louds_sparse.cpp
+++ test/unitTest/test_louds_sparse.cpp
@@ -24,12 +24,12 @@ namespace surf {
         static const SuffixType kSuffixTypeList[kNumSuffixType] = {kNone, kHash, kReal, kMixed};
         static const int kNumSuffixLen = 6;
         static const level_t kSuffixLenList[kNumSuffixLen] = {1, 3, 7, 8, 13, 26};
-        static std::vector<std::string> words;
+        static std::vector<std::pair<std::vector<label_t>,uint64_t>> words;
 
         class SparseUnitTest : public ::testing::Test {
         public:
             virtual void SetUp() {
-                truncateWordSuffixes();
+                truncateSuffixes(words,words_trunc_);
                 fillinInts();
                 data_ = nullptr;
             }
@@ -41,7 +41,8 @@ namespace surf {
 
             void newBuilder(SuffixType suffix_type, level_t suffix_len);
 
-            void truncateWordSuffixes();
+            void truncateSuffixes(const std::vector<std::pair<std::vector<label_t>,uint64_t>> &keys,
+                                      std::vector<std::pair<std::vector<label_t>,uint64_t>> &keys_trunc);
 
             void fillinInts();
 
@@ -51,14 +52,14 @@ namespace surf {
 
             SuRFBuilder *builder_;
             LoudsSparse *louds_sparse_;
-            std::vector<std::string> words_trunc_;
-            std::vector<std::string> ints_;
+            std::vector<std::pair<std::vector<label_t>,uint64_t>> words_trunc_;
+            std::vector<std::pair<std::vector<label_t>,uint64_t>> ints_;
             char *data_;
         };
 
-        static int getCommonPrefixLen(const std::string &a, const std::string &b) {
+        static int getCommonPrefixLen(const std::vector<label_t> &a, const std::vector<label_t> &b) {
             int len = 0;
-            while ((len < (int) a.length()) && (len < (int) b.length()) && (a[len] == b[len]))
+            while ((len < (int) a.size()) && (len < (int) b.size()) && (a[len] == b[len]))
                 len++;
             return len;
         }
@@ -82,32 +83,35 @@ namespace surf {
                 builder_ = new SuRFBuilder(kIncludeDense, kSparseDenseRatio, kNone, 0, 0);
         }
 
-        void SparseUnitTest::truncateWordSuffixes() {
-            assert(words.size() > 1);
-
+        void SparseUnitTest::truncateSuffixes(const std::vector<std::pair<std::vector<label_t>,uint64_t>> &keys,
+                                                   std::vector<std::pair<std::vector<label_t>,uint64_t>> &keys_trunc) {
             int commonPrefixLen = 0;
-            for (unsigned i = 0; i < words.size(); i++) {
+            for (unsigned i = 0; i < keys.size(); i++) {
                 if (i == 0) {
-                    commonPrefixLen = getCommonPrefixLen(words[i], words[i + 1]);
-                } else if (i == words.size() - 1) {
-                    commonPrefixLen = getCommonPrefixLen(words[i - 1], words[i]);
+                    commonPrefixLen = getCommonPrefixLen(keys[i].first, keys[i + 1].first);
+                } else if (i == keys.size() - 1) {
+                    commonPrefixLen = getCommonPrefixLen(keys[i - 1].first, keys[i].first);
                 } else {
-                    commonPrefixLen = getMax(getCommonPrefixLen(words[i - 1], words[i]),
-                                             getCommonPrefixLen(words[i], words[i + 1]));
+                    commonPrefixLen = getMax(getCommonPrefixLen(keys[i - 1].first, keys[i].first),
+                                             getCommonPrefixLen(keys[i].first, keys[i + 1].first));
                 }
 
-                if (commonPrefixLen < (int) words[i].length()) {
-                    words_trunc_.push_back(words[i].substr(0, commonPrefixLen + 1));
+                if (commonPrefixLen < (int) keys[i].first.size()) {
+                    std::vector<label_t> subVector;
+                    for (int j=0; j<commonPrefixLen + 1; j++) {
+                        subVector.emplace_back(keys[i].first[j]);
+                    }
+                    keys_trunc.push_back({subVector,keys[i].second});
                 } else {
-                    words_trunc_.push_back(words[i]);
-                    words_trunc_[i] += (char) kTerminator;
+                    keys_trunc.push_back(keys[i]);
+                    keys_trunc[i].first.emplace_back(kTerminator);
                 }
             }
         }
 
         void SparseUnitTest::fillinInts() {
             for (uint64_t i = 0; i < kIntTestBound; i += kIntTestSkip) {
-                ints_.push_back(uint64ToString(i));
+                ints_.push_back({uint64ToByteVector(i),i});
             }
         }
 
@@ -130,15 +134,15 @@ namespace surf {
         void SparseUnitTest::testLookupWord() {
             position_t in_node_num = 0;
             for (unsigned i = 0; i < words.size(); i++) {
-                bool key_exist = louds_sparse_->lookupKey(words[i], in_node_num);
+                bool key_exist = louds_sparse_->lookupKey(words[i].first, in_node_num).has_value();
                 ASSERT_TRUE(key_exist);
             }
 
             for (unsigned i = 0; i < words.size(); i++) {
-                for (unsigned j = 0; j < words_trunc_[i].size() && j < words[i].size(); j++) {
-                    std::string key = words[i];
+                for (unsigned j = 0; j < words_trunc_[i].first.size() && j < words[i].first.size(); j++) {
+                    std::vector<label_t> key = words[i].first;
                     key[j] = 'A';
-                    bool key_exist = louds_sparse_->lookupKey(key, in_node_num);
+                    bool key_exist = louds_sparse_->lookupKey(key, in_node_num).has_value();
                     ASSERT_FALSE(key_exist);
                 }
             }
@@ -148,15 +152,7 @@ namespace surf {
             for (int t = 0; t < kNumSuffixType; t++) {
                 for (int k = 0; k < kNumSuffixLen; k++) {
                     newBuilder(kSuffixTypeList[t], kSuffixLenList[k]);
-                    std::vector< std::vector<label_t>> keys;
-                    for (const std::string &keyStr : words) {
-                        std::vector<label_t> key;
-                        for (int i=0; i<keyStr.length(); i++) {
-                            key.emplace_back(keyStr[i]);
-                        }
-                        keys.emplace_back(key);
-                    }
-                    builder_->build(keys);
+                    builder_->build(words);
                     louds_sparse_ = new LoudsSparse(builder_);
 
                     testLookupWord();
@@ -171,15 +167,7 @@ namespace surf {
             for (int t = 0; t < kNumSuffixType; t++) {
                 for (int k = 0; k < kNumSuffixLen; k++) {
                     newBuilder(kSuffixTypeList[t], kSuffixLenList[k]);
-                    std::vector< std::vector<label_t>> keys;
-                    for (const std::string &keyStr : words) {
-                        std::vector<label_t> key;
-                        for (int i=0; i<keyStr.length(); i++) {
-                            key.emplace_back(keyStr[i]);
-                        }
-                        keys.emplace_back(key);
-                    }
-                    builder_->build(keys);
+                    builder_->build(words);
                     louds_sparse_ = new LoudsSparse(builder_);
 
                     testSerialize();
@@ -191,20 +179,12 @@ namespace surf {
 
         TEST_F (SparseUnitTest, lookupIntTest) {
             newBuilder(kReal, 8);
-            std::vector< std::vector<label_t>> keys;
-            for (const std::string &keyStr : ints_) {
-                std::vector<label_t> key;
-                for (int i=0; i<keyStr.length(); i++) {
-                    key.emplace_back(keyStr[i]);
-                }
-                keys.emplace_back(key);
-            }
-            builder_->build(keys);
+            builder_->build(ints_);
             louds_sparse_ = new LoudsSparse(builder_);
             position_t in_node_num = 0;
 
             for (uint64_t i = 0; i < kIntTestBound; i += kIntTestSkip) {
-                bool key_exist = louds_sparse_->lookupKey(uint64ToString(i), in_node_num);
+                bool key_exist = louds_sparse_->lookupKey(uint64ToString(i), in_node_num).has_value();
                 if (i % kIntTestSkip == 0)
                     ASSERT_TRUE(key_exist);
                 else
@@ -219,15 +199,7 @@ namespace surf {
             for (int t = 0; t < kNumSuffixType; t++) {
                 for (int k = 0; k < kNumSuffixLen; k++) {
                     newBuilder(kSuffixTypeList[t], kSuffixLenList[k]);
-                    std::vector< std::vector<label_t>> keys;
-                    for (const std::string &keyStr : words) {
-                        std::vector<label_t> key;
-                        for (int i=0; i<keyStr.length(); i++) {
-                            key.emplace_back(keyStr[i]);
-                        }
-                        keys.emplace_back(key);
-                    }
-                    builder_->build(keys);
+                    builder_->build(words);
                     louds_sparse_ = new LoudsSparse(builder_);
 
                     bool inclusive = true;
@@ -236,27 +208,24 @@ namespace surf {
                             inclusive = false;
                         for (unsigned j = 0; j < words.size() - 1; j++) {
                             LoudsSparse::Iter iter(louds_sparse_);
-                            bool could_be_fp = louds_sparse_->moveToKeyGreaterThan(words[j], inclusive, iter);
+                            bool could_be_fp = louds_sparse_->moveToKeyGreaterThan(words[j].first, inclusive, iter);
 
                             ASSERT_TRUE(iter.isValid());
                             std::string iter_key = iter.getKey();
-                            std::string word_prefix_fp = words[j].substr(0, iter_key.length());
-                            std::string word_prefix_true = words[j + 1].substr(0, iter_key.length());
                             bool is_prefix = false;
                             if (could_be_fp)
-                                is_prefix = (word_prefix_fp.compare(iter_key) == 0);
+                                is_prefix = isSameKey(stringToByteVector(iter_key),words[j].first,iter_key.length());
                             else
-                                is_prefix = (word_prefix_true.compare(iter_key) == 0);
+                                is_prefix = isSameKey(stringToByteVector(iter_key),words[j + 1].first,iter_key.length());
                             ASSERT_TRUE(is_prefix);
                         }
 
                         LoudsSparse::Iter iter(louds_sparse_);
-                        bool could_be_fp = louds_sparse_->moveToKeyGreaterThan(words[words.size() - 1], inclusive,
+                        bool could_be_fp = louds_sparse_->moveToKeyGreaterThan(words[words.size() - 1].first, inclusive,
                                                                                iter);
                         if (could_be_fp) {
                             std::string iter_key = iter.getKey();
-                            std::string word_prefix_fp = words[words.size() - 1].substr(0, iter_key.length());
-                            bool is_prefix = (word_prefix_fp.compare(iter_key) == 0);
+                            bool is_prefix = isSameKey(stringToByteVector(iter_key),words[words.size() - 1].first,iter_key.length());
                             ASSERT_TRUE(is_prefix);
                         } else {
                             ASSERT_FALSE(iter.isValid());
@@ -272,15 +241,7 @@ namespace surf {
 
         TEST_F (SparseUnitTest, moveToKeyGreaterThanIntTest) {
             newBuilder(kReal, 8);
-            std::vector< std::vector<label_t>> keys;
-            for (const std::string &keyStr : ints_) {
-                std::vector<label_t> key;
-                for (int i=0; i<keyStr.length(); i++) {
-                    key.emplace_back(keyStr[i]);
-                }
-                keys.emplace_back(key);
-            }
-            builder_->build(keys);
+            builder_->build(ints_);
             louds_sparse_ = new LoudsSparse(builder_);
 
             bool inclusive = true;
@@ -326,25 +287,16 @@ namespace surf {
 
         TEST_F (SparseUnitTest, IteratorIncrementWordTest) {
             newBuilder(kReal, 8);
-            std::vector< std::vector<label_t>> keys;
-            for (const std::string &keyStr : words) {
-                std::vector<label_t> key;
-                for (int i=0; i<keyStr.length(); i++) {
-                    key.emplace_back(keyStr[i]);
-                }
-                keys.emplace_back(key);
-            }
-            builder_->build(keys);
+            builder_->build(words);
             louds_sparse_ = new LoudsSparse(builder_);
             bool inclusive = true;
             LoudsSparse::Iter iter(louds_sparse_);
-            louds_sparse_->moveToKeyGreaterThan(words[0], inclusive, iter);
+            louds_sparse_->moveToKeyGreaterThan(words[0].first, inclusive, iter);
             for (unsigned i = 1; i < words.size(); i++) {
                 iter++;
                 ASSERT_TRUE(iter.isValid());
                 std::string iter_key = iter.getKey();
-                std::string word_prefix = words[i].substr(0, iter_key.length());
-                bool is_prefix = (word_prefix.compare(iter_key) == 0);
+                bool is_prefix = isSameKey(stringToByteVector(iter_key),words[i].first,iter_key.length());
                 ASSERT_TRUE(is_prefix);
             }
             iter++;
@@ -356,15 +308,7 @@ namespace surf {
 
         TEST_F (SparseUnitTest, IteratorIncrementIntTest) {
             newBuilder(kReal, 8);
-            std::vector< std::vector<label_t>> keys;
-            for (const std::string &keyStr : ints_) {
-                std::vector<label_t> key;
-                for (int i=0; i<keyStr.length(); i++) {
-                    key.emplace_back(keyStr[i]);
-                }
-                keys.emplace_back(key);
-            }
-            builder_->build(keys);
+            builder_->build(ints_);
             louds_sparse_ = new LoudsSparse(builder_);
             bool inclusive = true;
             LoudsSparse::Iter iter(louds_sparse_);
@@ -386,25 +330,16 @@ namespace surf {
 
         TEST_F (SparseUnitTest, IteratorDecrementWordTest) {
             newBuilder(kReal, 8);
-            std::vector< std::vector<label_t>> keys;
-            for (const std::string &keyStr : words) {
-                std::vector<label_t> key;
-                for (int i=0; i<keyStr.length(); i++) {
-                    key.emplace_back(keyStr[i]);
-                }
-                keys.emplace_back(key);
-            }
-            builder_->build(keys);
+            builder_->build(words);
             louds_sparse_ = new LoudsSparse(builder_);
             bool inclusive = true;
             LoudsSparse::Iter iter(louds_sparse_);
-            louds_sparse_->moveToKeyGreaterThan(words[words.size() - 1], inclusive, iter);
+            louds_sparse_->moveToKeyGreaterThan(words[words.size() - 1].first, inclusive, iter);
             for (int i = words.size() - 2; i >= 0; i--) {
                 iter--;
                 ASSERT_TRUE(iter.isValid());
                 std::string iter_key = iter.getKey();
-                std::string word_prefix = words[i].substr(0, iter_key.length());
-                bool is_prefix = (word_prefix.compare(iter_key) == 0);
+                bool is_prefix = isSameKey(stringToByteVector(iter_key),words[i].first,iter_key.length());
                 ASSERT_TRUE(is_prefix);
             }
             iter--;
@@ -416,15 +351,7 @@ namespace surf {
 
         TEST_F (SparseUnitTest, IteratorDecrementIntTest) {
             newBuilder(kReal, 8);
-            std::vector< std::vector<label_t>> keys;
-            for (const std::string &keyStr : ints_) {
-                std::vector<label_t> key;
-                for (int i=0; i<keyStr.length(); i++) {
-                    key.emplace_back(keyStr[i]);
-                }
-                keys.emplace_back(key);
-            }
-            builder_->build(keys);
+            builder_->build(ints_);
             louds_sparse_ = new LoudsSparse(builder_);
             bool inclusive = true;
             LoudsSparse::Iter iter(louds_sparse_);
@@ -447,30 +374,20 @@ namespace surf {
 
         TEST_F (SparseUnitTest, FirstAndLastLabelInRootTest) {
             newBuilder(kReal, 8);
-            std::vector< std::vector<label_t>> keys;
-            for (const std::string &keyStr : words) {
-                std::vector<label_t> key;
-                for (int i=0; i<keyStr.length(); i++) {
-                    key.emplace_back(keyStr[i]);
-                }
-                keys.emplace_back(key);
-            }
-            builder_->build(keys);
+            builder_->build(words);
             louds_sparse_ = new LoudsSparse(builder_);
             LoudsSparse::Iter iter(louds_sparse_);
             iter.setToFirstLabelInRoot();
             iter.moveToLeftMostKey();
             std::string iter_key = iter.getKey();
-            std::string word_prefix = words[0].substr(0, iter_key.length());
-            bool is_prefix = (word_prefix.compare(iter_key) == 0);
+            bool is_prefix = isSameKey(stringToByteVector(iter_key),words[0].first,iter_key.length());
             ASSERT_TRUE(is_prefix);
 
             iter.clear();
             iter.setToLastLabelInRoot();
             iter.moveToRightMostKey();
             iter_key = iter.getKey();
-            word_prefix = words[kWordTestSize - 1].substr(0, iter_key.length());
-            is_prefix = (word_prefix.compare(iter_key) == 0);
+            is_prefix = isSameKey(stringToByteVector(iter_key),words[kWordTestSize - 1].first,iter_key.length());
             ASSERT_TRUE(is_prefix);
 
             delete builder_;
@@ -480,11 +397,15 @@ namespace surf {
 
         void loadWordList() {
             std::ifstream infile(kFilePath);
-            std::string key;
+            std::string keyStr;
             int count = 0;
             while (infile.good() && count < kWordTestSize) {
-                infile >> key;
-                words.push_back(key);
+                infile >> keyStr;
+                std::vector<label_t> key;
+                for (int i=0; i<keyStr.length(); i++) {
+                    key.emplace_back(keyStr[i]);
+                }
+                words.push_back({key,count});
                 count++;
             }
         }
